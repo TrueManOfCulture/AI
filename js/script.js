b7ebd186f91c40f9fc562a35c8b3f92dae45db59
Kikoszzz <52199358+Kikoszzz@users.noreply.github.com>
2023-12-18 12:12:00 +0000

    Add files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function teste(){
+    var paragraph = document.getElementById('teste');
+    var valorAtual = parseInt(paragraph.innerHTML, 10);
+    var novoValor = valorAtual + 1;
+    paragraph.innerHTML = novoValor;
+
+    var force = 1;
+    
+}
