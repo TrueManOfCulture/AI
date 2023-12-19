29499ba1633c13bbb4b036c49dbf92ff2981a32f
Kikoszzz <52199358+Kikoszzz@users.noreply.github.com>
2023-12-19 14:59:51 +0000

    React

--- js/react.js
+++ js/react.js
@@ -1,48 +1,48 @@
 class FORMULARIO extends React.Component {
-    render() {
-      return React.createElement('form', null,
-        React.createElement('label', { htmlFor: 'name' }, 'Nome'),
-        React.createElement('br', null),
-        React.createElement('input', {
-          type: 'text',
-          id: 'name',
-          placeholder: 'Primeiro e Último nome',
-          pattern: '[A-Za-zÀ-úçÇ\\s]*',
-          autoComplete: 'name',
-          required: true,
-          className: 'largura-estendida',
-        }),
-        React.createElement('br', null), React.createElement('br', null),
-        React.createElement('label', { htmlFor: 'email' }, 'Email'), React.createElement('br', null),
-        React.createElement('input', {
-          type: 'email',
-          id: 'email',
-          placeholder: 'email@exemplo.com',
-          autoComplete: 'email',
-          required: true,
-          className: 'largura-estendida',
-        }),
-        React.createElement('br', null), React.createElement('br', null),
-        React.createElement('label', { htmlFor: 'assunto' }, 'Assunto'), React.createElement('br', null),
-        React.createElement('input', {
-          type: 'text',
-          id: 'assunto',
-          placeholder: 'Assunto',
-          pattern: '[^\\n]',
-          required: true,
-          className: 'largura-estendida',
-        }),
-        React.createElement('br', null), React.createElement('br', null),
-        React.createElement('label', { htmlFor: 'mensagem' }, 'Mensagem'), React.createElement('br', null),
-        React.createElement('textarea', {
-          cols: '100',
-          rows: '10',
-          placeholder: 'Escreva aqui...',
-          required: true,
-        })
-      );
-    }
+  render() {
+    return React.createElement('form', null,
+      React.createElement('label', { htmlFor: 'name' }, 'Nome'),
+      React.createElement('br', null),
+      React.createElement('input', {
+        type: 'text',
+        id: 'name',
+        placeholder: 'Primeiro e Último nome',
+        pattern: '[A-Za-zÀ-úçÇ\\s]*',
+        autoComplete: 'name',
+        required: true,
+        className: 'largura-estendida',
+      }),
+      React.createElement('br', null), React.createElement('br', null),
+      React.createElement('label', { htmlFor: 'email' }, 'Email'), React.createElement('br', null),
+      React.createElement('input', {
+        type: 'email',
+        id: 'email',
+        placeholder: 'email@exemplo.com',
+        autoComplete: 'email',
+        required: true,
+        className: 'largura-estendida',
+      }),
+      React.createElement('br', null), React.createElement('br', null),
+      React.createElement('label', { htmlFor: 'assunto' }, 'Assunto'), React.createElement('br', null),
+      React.createElement('input', {
+        type: 'text',
+        id: 'assunto',
+        placeholder: 'Assunto',
+        pattern: '[^\\n]',
+        required: true,
+        className: 'largura-estendida',
+      }),
+      React.createElement('br', null), React.createElement('br', null),
+      React.createElement('label', { htmlFor: 'mensagem' }, 'Mensagem'), React.createElement('br', null),
+      React.createElement('textarea', {
+        cols: '100',
+        rows: '10',
+        placeholder: 'Escreva aqui...',
+        required: true,
+      })
+    );
   }
+}
   
 document.addEventListener('DOMContentLoaded', function () {
   ReactDOM.render(React.createElement(FORMULARIO), document.getElementById('TEST'));
