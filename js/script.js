f40f291ac6b50dad3385c0c95e55055a784c6f07
Kikoszzz <52199358+Kikoszzz@users.noreply.github.com>
2024-01-27 20:08:47 +0000

    .

--- js/script.js
+++ js/script.js
@@ -22,12 +22,18 @@ window.onload = function() {
     HoraEData(); // Executa a função para exibir a hora e a data
     checkHeaderPosition(); // Executa a função para verificar a posição do cabeçalho
     
-    const menu_btn = document.querySelector('.hamburger');
-    const mobile_menu = document.querySelector('.mobile-nav');
+    const hamburger = document.querySelector('.hamburger');
+    const mobileNav = document.querySelector('.mobile-nav');
+    const navLinks = document.querySelectorAll('.mobile-nav a');
 
-    menu_btn.addEventListener('click', function () {
-        menu_btn.classList.toggle('is-active');
-        mobile_menu.classList.toggle('is-active');
+    hamburger.addEventListener('click', function() {
+        mobileNav.classList.toggle('is-active');
+    });
+
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function() {
+            mobileNav.classList.remove('is-active');
+        });
     });
 };
 
