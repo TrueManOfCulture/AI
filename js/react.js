6b5c97dcb5a71097a941929ab2d7cc97c4b9652a
TrueManOfCulture <88104776+TrueManOfCulture@users.noreply.github.com>
2024-01-27 21:12:03 +0000

    react falta

--- js/react.js
+++ js/react.js
@@ -5,8 +5,7 @@ class FORMULARIO extends React.Component {
         paddingTop: '10px', // Correção: use camelCase para propriedades do CSS
       }, 
     },
-      React.createElement('label', { htmlFor: 'name' }, 'Nome'),
-      React.createElement('br', null),
+      React.createElement('label', { htmlFor: 'name' }, 'Nome :'),
       React.createElement('input', {
         type: 'text',
         id: 'name',
@@ -15,9 +14,9 @@ class FORMULARIO extends React.Component {
         autoComplete: 'name',
         required: true,
         className: 'largura-estendida',
+        style : { width: '150px', padding: '5px' },
       }),
-      React.createElement('br', null), React.createElement('br', null),
-      React.createElement('label', { htmlFor: 'email' }, 'Email'), React.createElement('br', null),
+      React.createElement('label', { htmlFor: 'email'}, 'Email :'),
       React.createElement('input', {
         type: 'email',
         id: 'email',
@@ -25,26 +24,40 @@ class FORMULARIO extends React.Component {
         autoComplete: 'email',
         required: true,
         className: 'largura-estendida',
+        style : { width: '300px', padding: '5px' },
       }),
       React.createElement('br', null), React.createElement('br', null),
-      React.createElement('label', { htmlFor: 'assunto' }, 'Assunto'), React.createElement('br', null),
+      React.createElement('label', { htmlFor: 'assunto' }, 'Assunto:'), React.createElement('br', null),
       React.createElement('input', {
         type: 'text',
         id: 'assunto',
         placeholder: 'Assunto',
         pattern: '[^\\n]',
         required: true,
+        style: { width: '90%', padding: '5px'},
         className: 'largura-estendida',
       }),
       React.createElement('br', null), React.createElement('br', null),
       React.createElement('label', { htmlFor: 'mensagem' }, 'Mensagem'), React.createElement('br', null),
       React.createElement('textarea', {
-        placeholder: 'Escreva aqui...',
+        id: 'mensagem',
+        placeholder: 'Mensagem',
+        pattern: '[A-Za-zÀ-úçÇ\\s]*',
+        autoComplete: '0000000',
         required: true,
-        style: {
-          marginBottom: '10px', // Correção: use camelCase para propriedades do CSS
-        },
-      })
+        className: 'Mensagem',
+        style: { width: '90%', padding: '5px', height: '150px' },
+      }),
+      React.createElement('br', null), React.createElement('br', null),
+      React.createElement('input', {
+        type : 'button',
+        value : "Clear",
+        id : "clearbutton",
+      },null),
+      React.createElement('input', {
+        type : 'submit',
+        id : "submitbutton",
+      }),
     );
   }
 }
