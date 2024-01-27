a6baf55c88dfcfe8e6f87f13984ffe3771ea1a59
Kikoszzz <52199358+Kikoszzz@users.noreply.github.com>
2024-01-27 17:54:17 +0000

    .

--- js/script.js
+++ js/script.js
@@ -12,13 +12,23 @@ function checkHeaderPosition() {
         header.style.position = "fixed";
         header.style.top = "0";
     } else {
-        header.style.position = "sticky";
+        header.style.position = "relative";
     }
 }
+
+
 
 window.onload = function() {
     HoraEData(); // Executa a função para exibir a hora e a data
     checkHeaderPosition(); // Executa a função para verificar a posição do cabeçalho
+    
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav');
+
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    });
 };
 
 window.onscroll = function() {
